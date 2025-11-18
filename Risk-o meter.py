fb2271ec31a4ee099013610525fcfcbdbbd6ba27
riddhishmalekar55-cyber <riddhishmalekar55@gmail.com>
2025-11-18 14:50:57 +0530

    Update Risk-o meter.py

--- Risk-o meter.py
+++ Risk-o meter.py
@@ -1,8 +1,4 @@
-import streamlit as st
-import yfinance as yf
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+
 
 import streamlit as st
 import yfinance as yf
